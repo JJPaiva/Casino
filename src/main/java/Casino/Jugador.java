65d12b39daa083d034218d39dce6a27bf5ac9d38
Juan Paiva <j.paiva01@ufromail.cl>
2023-06-15 23:18:02 -0400

    Update Jugador.java

--- src/main/java/Casino/Jugador.java
+++ src/main/java/Casino/Jugador.java
@@ -1,5 +1,36 @@
 package Casino;
 
 public class Jugador {
-    protected String nombre;
+    private String nombre;
+    private int dinero=50000;
+
+
+    public Jugador(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDinero() {
+        return dinero;
+    }
+
+    public void setDinero(int dinero) {
+        this.dinero = dinero;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre Jugador= " + nombre + '\n' + "Dinero restante= " + dinero;
+    }
+
+    public String toCSV(){
+        return this.nombre+","+this.dinero;
+    }
 }
